b3736bc9a6dddfc1d6679a60ee82faf621c84b73
Poom <pittayatorn_p@hotmail.com>
2025-08-02 10:41:09 +0700

    v1

---
+++ Samkong/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using Samkong.AppContext;
+using Samkong.Repo.IRepo;
+using Samkong.Repo.RepoService;
+
+namespace Samkong
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+        public ICustomerRepository Customer { get; }
+        public IEmployeeRepository Employee { get; }
+        public IProductRepository Product { get;}
+        public UnitOfWork(ApplicationDbContext context, ICustomerRepository customer, IEmployeeRepository employee, IProductRepository product)
+        {
+            _context = context;
+            Customer = customer;
+            Employee = employee;
+            Product = product;
+        }
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+    }
+}
